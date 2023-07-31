b0a525a00dbbf3cc314ba2f1b510255955715d05
Elena Morilova <elena.morilova@mail.ru>
2023-07-31 23:11:21 +0400

    add changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,7 @@ import schemas.schemas_api
 from models.api import get_list_users, post_create_users, update_user, \
     get_user, delete_user, post_register_user
 from pytest_voluptuous import S
-from models import data
+
 
 
 @allure.tag('api')
@@ -12,11 +12,11 @@ from models import data
 @allure.feature('Проверка метода на возвращение списка пользователей')
 @allure.link(f'/api/users')
 def test_list_users():
-    with allure.step(f'  GET запрос c параметром page={data.page}'):
-        response = get_list_users(params={"page": data.page})
+    with allure.step(f'  GET запрос c параметром page=2'):
+        response = get_list_users(params={"page": 2})
     with allure.step('Проверка статус-кода и списка пользователей'):
         assert response.status_code == 200
-        assert response.json()['page'] == data.page
+        assert response.json()['page'] == 2
 
 
 @allure.tag('api')
@@ -24,7 +24,7 @@ def test_list_users():
 @allure.label('owner', 'Morilova')
 @allure.feature('Проверка создания пользователя при вводе валидных данных')
 @allure.link(f'/api/users')
-def test_create_user(name=data.create_name, job=data.create_job):
+def test_create_user(name='Nina', job='postman'):
     with allure.step(f'POST запрос c валидными данными в теле запроса'):
         response = post_create_users(name, job)
     with allure.step('Проверка успешного создания и соответствия введенных данных'):
@@ -38,12 +38,12 @@ def test_create_user(name=data.create_name, job=data.create_job):
 @allure.tag('api')
 @allure.severity(Severity.CRITICAL)
 @allure.label('owner', 'Morilova')
-@allure.feature(f'Проверка возвращения в ответе пользователя по заданному id {data.id_user}')
-@allure.link(f'/api/users/{data.id_user}')
-def test_get_user(id_user=data.id_user):
+@allure.feature(f'Проверка возвращения в ответе пользователя по заданному id {6}')
+@allure.link(f'/api/users/{6}')
+def test_get_user(id_user=6):
     with allure.step(f'GET запрос c id пользователя'):
         response = get_user(id_user)
-    with allure.step(f'Проверка получения в ответе данных пользователя с id {data.id_user}'):
+    with allure.step(f'Проверка получения в ответе данных пользователя с id {6}'):
         assert response.status_code == 200
         assert response.json()['data']['id'] == 6
         assert response.json()['data']['email'] == 'tracey.ramos@reqres.in'
@@ -57,9 +57,9 @@ def test_get_user(id_user=data.id_user):
 @allure.severity(Severity.CRITICAL)
 @allure.label('owner', 'Morilova')
 @allure.feature('Проверка обновления id, name, job пользователя')
-@allure.link(f'/api/users/{data.id_user_update}')
-def test_update(id_user=data.id_user_update, name=data.name_update,
-                job=data.job_update):
+@allure.link(f'/api/users/{4}')
+def test_update(id_user=4, name='Egor',
+                job='driver'):
     with allure.step('POST запрос с валидными данными'):
         response = update_user(id_user, name, job)
     with allure.step('Проверка успешного обновления пользователя'):
@@ -74,9 +74,9 @@ def test_update(id_user=data.id_user_update, name=data.name_update,
 @allure.severity(Severity.CRITICAL)
 @allure.label('owner', 'Morilova')
 @allure.feature('Проверка удаления пользователя')
-@allure.link(f'/api/users/{data.user_id_delete}')
-def test_delete(id_user=data.user_id_delete):
-    with allure.step(f'Отправка запроса DELETE с id {data.user_id_delete}'):
+@allure.link(f'/api/users/4')
+def test_delete(id_user=4):
+    with allure.step(f'Отправка запроса DELETE с id 4'):
         response = delete_user(id_user)
     with allure.step('Проверка соответствия статус-коду'):
         assert response.status_code == 204
